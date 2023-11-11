fb1b51a6d1fea31b20cd871521c9143633e4dcbe
noname512 <zxononame@gmail.com>
2023-11-11 10:03:24 -0500

    修复微光之触结束后不给光的bug

--- src/main/java/nearlmod/powers/GlimmeringTouchPower.java
+++ src/main/java/nearlmod/powers/GlimmeringTouchPower.java
@@ -3,6 +3,7 @@ package nearlmod.powers;
 import basemod.interfaces.CloneablePowerInterface;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
@@ -13,6 +14,8 @@ import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.AbstractPower;
+import nearlmod.orbs.AbstractFriend;
+
 import java.util.ArrayList;
 
 public class GlimmeringTouchPower extends AbstractPower implements CloneablePowerInterface {
@@ -49,6 +52,7 @@ public class GlimmeringTouchPower extends AbstractPower implements CloneablePowe
                 AbstractDungeon.actionManager.addToBottom(new DamageAction(ms, new DamageInfo(owner, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.FIRE));
             }
             AbstractDungeon.actionManager.addToBottom(new RemoveSpecificPowerAction(owner, owner, this));
+            AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(owner, owner, new LightPower(owner, 10)));
         }
         updateDescription();
     }
