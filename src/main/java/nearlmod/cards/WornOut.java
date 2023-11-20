e9a601bd3dc441a3e622f126d9cfab397df28465
noname512 <zxononame@gmail.com>
2023-11-20 08:56:44 -0500

    新增卡牌 大道磨灭

---
+++ src/main/java/nearlmod/cards/WornOut.java
@@ -0,0 +1,47 @@
+package nearlmod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import nearlmod.patches.AbstractCardEnum;
+import nearlmod.powers.CostNextTurnPower;
+
+public class WornOut extends AbstractNearlCard {
+    public static final String ID = "nearlmod:WornOut";
+    public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    public static final String IMG_PATH = "images/cards/secondsun.png";
+    private static final int COST = 1;
+    private static final int COST_GAIN = 2;
+    private static final int UPGRADE_PLUS_COST = 1;
+
+    public WornOut() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
+                CardType.SKILL, AbstractCardEnum.NEARL_GOLD,
+                CardRarity.COMMON, CardTarget.SELF);
+        magicNumber = baseMagicNumber = COST_GAIN;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(p, p, new CostNextTurnPower(p, magicNumber)));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new WornOut();
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_PLUS_COST);
+        }
+    }
+}
