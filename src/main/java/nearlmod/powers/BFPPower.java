fe90b3add59c26642c5292d03113bd7a5659412c
noname512 <zxononame@gmail.com>
2023-11-20 14:54:01 -0500

    黑恶魔的庇护检测是否为第一张攻击牌

--- src/main/java/nearlmod/powers/BFPPower.java
+++ src/main/java/nearlmod/powers/BFPPower.java
@@ -3,6 +3,8 @@ package nearlmod.powers;
 import basemod.interfaces.CloneablePowerInterface;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -12,12 +14,14 @@ import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import com.megacrit.cardcrawl.powers.VulnerablePower;
 import com.megacrit.cardcrawl.powers.WeakPower;
+import nearlmod.patches.NearlTags;
 
 public class BFPPower extends AbstractPower implements CloneablePowerInterface {
     public static final String POWER_ID = "nearlmod:BFPPower";
     public static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+    private int cardPlayed;
 
     public BFPPower(AbstractCreature owner) {
         name = NAME;
@@ -26,6 +30,7 @@ public class BFPPower extends AbstractPower implements CloneablePowerInterface {
         region128 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage("images/powers/light power 84.png"), 0, 0, 84, 84);
         region48 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage("images/powers/light power 32.png"), 0, 0, 32, 32);
         type = PowerType.BUFF;
+        cardPlayed = 0;
         updateDescription();
     }
 
@@ -41,12 +46,25 @@ public class BFPPower extends AbstractPower implements CloneablePowerInterface {
 
     @Override
     public void onAttack(DamageInfo info, int damageAmount, AbstractCreature target) {
+        if (cardPlayed > 1) return;
         if (info.type != DamageInfo.DamageType.NORMAL) return;
+        if (info.name != null && info.name.endsWith("FriendDmg")) return;
         int effectType = AbstractDungeon.cardRandomRng.random(1, 2);
         if (owner.hasPower("nearlmod:WFPPower")) effectType = 3;
         if ((effectType & 1) != 0)
             addToBot(new ApplyPowerAction(target, info.owner, new WeakPower(target, 1, false)));
         if ((effectType & 2) != 0)
             addToBot(new ApplyPowerAction(target, info.owner, new VulnerablePower(target, 1, false)));
+    }
+
+    @Override
+    public void onUseCard(AbstractCard card, UseCardAction action) {
+        if (card.hasTag(NearlTags.IS_FRIEND_CARD) || card.type != AbstractCard.CardType.ATTACK) return;
+        cardPlayed++;
+    }
+
+    @Override
+    public void atStartOfTurn() {
+        cardPlayed = 0;
     }
 }
