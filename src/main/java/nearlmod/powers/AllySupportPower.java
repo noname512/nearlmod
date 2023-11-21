ca2e52a5b57261869b88cf398ddaef0b775dbc3a
noname512 <zxononame@gmail.com>
2023-11-21 13:47:42 -0500

    fix：同盟支援判定伤害类型

--- src/main/java/nearlmod/powers/AllySupportPower.java
+++ src/main/java/nearlmod/powers/AllySupportPower.java
@@ -33,6 +33,7 @@ public class AllySupportPower extends AbstractPower implements CloneablePowerInt
     @Override
     public void onAttack(DamageInfo info, int damageAmount, AbstractCreature target) {
         if (info.name != null && info.name.startsWith(Fartooth.ORB_ID)) return;
+        if (info.type != DamageInfo.DamageType.NORMAL) return;
         for (AbstractOrb orb : ((AbstractPlayer)this.owner).orbs)
             if (orb instanceof Fartooth) {
                 addToBot(new DamageAction(target, new DamageInfo(null, amount + ((Fartooth) orb).trustAmount, DamageInfo.DamageType.THORNS)));
