17fd69a911bd87e37d687627957f01b2d0d98da0
noname512 <zxononame@gmail.com>
2023-11-21 14:23:04 -0500

    伙伴随本人一同转向

--- src/main/java/nearlmod/patches/SpireShieldCheckOrbsPatch.java
+++ src/main/java/nearlmod/patches/SpireShieldCheckOrbsPatch.java
@@ -12,22 +12,20 @@ import com.megacrit.cardcrawl.monsters.ending.SpireShield;
 import com.megacrit.cardcrawl.powers.StrengthPower;
 import nearlmod.characters.Nearl;
 
+@SpirePatch(clz = SpireShield.class, method = "takeTurn")
 public class SpireShieldCheckOrbsPatch {
-    @SpirePatch(clz = SpireShield.class, method = "takeTurn")
-    public static class checkOrbsPatch {
-        @SpirePrefixPatch
-        public static SpireReturn<?> Prefix(SpireShield __instance) {
-            if (!(AbstractDungeon.player instanceof Nearl))
-                return SpireReturn.Continue();
-            if (__instance.nextMove == 1) {
-                AbstractDungeon.actionManager.addToBottom(new ChangeStateAction(__instance, "ATTACK"));
-                AbstractDungeon.actionManager.addToBottom(new WaitAction(0.35F));
-                AbstractDungeon.actionManager.addToBottom(new DamageAction(AbstractDungeon.player, __instance.damage.get(0), AbstractGameAction.AttackEffect.BLUNT_HEAVY));
-                AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(AbstractDungeon.player, __instance, new StrengthPower(AbstractDungeon.player, -1), -1));
-                AbstractDungeon.actionManager.addToBottom(new RollMoveAction(__instance));
-                return SpireReturn.Return();
-            }
+    @SpirePrefixPatch
+    public static SpireReturn<?> Prefix(SpireShield __instance) {
+        if (!(AbstractDungeon.player instanceof Nearl))
             return SpireReturn.Continue();
+        if (__instance.nextMove == 1) {
+            AbstractDungeon.actionManager.addToBottom(new ChangeStateAction(__instance, "ATTACK"));
+            AbstractDungeon.actionManager.addToBottom(new WaitAction(0.35F));
+            AbstractDungeon.actionManager.addToBottom(new DamageAction(AbstractDungeon.player, __instance.damage.get(0), AbstractGameAction.AttackEffect.BLUNT_HEAVY));
+            AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(AbstractDungeon.player, __instance, new StrengthPower(AbstractDungeon.player, -1), -1));
+            AbstractDungeon.actionManager.addToBottom(new RollMoveAction(__instance));
+            return SpireReturn.Return();
         }
+        return SpireReturn.Continue();
     }
 }
