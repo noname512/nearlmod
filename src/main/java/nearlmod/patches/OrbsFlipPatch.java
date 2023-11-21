ea5f9c924e6f6ded0350db13ba1228b4137decbc
noname512 <zxononame@gmail.com>
2023-11-21 14:56:20 -0500

    矛盾死亡/药水攻击转向处理

--- src/main/java/nearlmod/patches/OrbsFlipPatch.java
+++ src/main/java/nearlmod/patches/OrbsFlipPatch.java
@@ -2,15 +2,48 @@ package nearlmod.patches;
 
 import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.ending.SpireShield;
+import com.megacrit.cardcrawl.monsters.ending.SpireSpear;
 import com.megacrit.cardcrawl.orbs.AbstractOrb;
+import com.megacrit.cardcrawl.ui.panels.PotionPopUp;
 import nearlmod.orbs.AbstractFriend;
 
-@SpirePatch(clz = AbstractPlayer.class, method = "playCard")
 public class OrbsFlipPatch {
-    @SpirePostfixPatch
-    public static void setFlip(AbstractPlayer __instance) {
-        for (AbstractOrb orb : __instance.orbs)
+    public static void setFlip(AbstractPlayer p) {
+        for (AbstractOrb orb : p.orbs)
             if (orb instanceof AbstractFriend)
-                ((AbstractFriend) orb).flipHorizontal = __instance.flipHorizontal;
+                ((AbstractFriend) orb).flipHorizontal = p.flipHorizontal;
+    }
+    @SpirePatch(clz = AbstractPlayer.class, method = "playCard")
+    public static class PlayCardPatch {
+        @SpirePostfixPatch
+        public static void setFlipOnCardPlayed(AbstractPlayer __instance) {
+            OrbsFlipPatch.setFlip(__instance);
+        }
+    }
+
+    @SpirePatch(clz = SpireSpear.class, method = "die")
+    public static class SpearDiePatch {
+        @SpirePostfixPatch
+        public static void setFlipOnSpireDead(SpireSpear __instance) {
+            OrbsFlipPatch.setFlip(AbstractDungeon.player);
+        }
+    }
+
+    @SpirePatch(clz = SpireShield.class, method = "die")
+    public static class ShieldDiePatch {
+        @SpirePostfixPatch
+        public static void setFlipOnShieldDead(SpireShield __instance) {
+            OrbsFlipPatch.setFlip(AbstractDungeon.player);
+        }
+    }
+
+    @SpirePatch(clz = PotionPopUp.class, method = "updateTargetMode")
+    public static class PotionPopUpPatch {
+        @SpirePostfixPatch
+        public static void setFlipOnPotionPopped(PotionPopUp __instance) {
+            OrbsFlipPatch.setFlip(AbstractDungeon.player);
+        }
     }
 }
