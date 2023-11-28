67f5d75e83b4dbb05de52a0ddd12f0c4de9d0bc3
noname512 <zxononame@gmail.com>
2023-11-27 19:48:05 -0500

    新增部分资源

--- src/main/java/nearlmod/cards/Dreadnought.java
+++ src/main/java/nearlmod/cards/Dreadnought.java
@@ -20,7 +20,7 @@ public class Dreadnought extends AbstractNearlCard {
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
-    public static final String IMG_PATH = "images/cards/nearlstrike.png";
+    public static final String IMG_PATH = "images/cards/dreadnought.png";
     private static final int COST = 1;
     private static final int ATTACK_DMG = 8;
     private static final int LIGHT_AMT = 5;
