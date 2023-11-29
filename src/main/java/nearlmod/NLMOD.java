858c781fa97fd97124f04608ff67c56b219131db
noname512 <zxononame@gmail.com>
2023-11-29 04:56:58 -0500

    新增卡牌：风暴反击

--- src/main/java/nearlmod/NLMOD.java
+++ src/main/java/nearlmod/NLMOD.java
@@ -198,6 +198,7 @@ public class NLMOD implements EditCardsSubscriber, EditCharactersSubscriber, Edi
         BaseMod.addCard(new RadianceConverging()); // 光耀积聚
         BaseMod.addCard(new Sacrifice()); // 牺牲
         BaseMod.addCard(new LightCascade()); // 光瀑
+        BaseMod.addCard(new StormCounterAttack()); // 风暴反击
 
         // Rare.
         BaseMod.addCard(new BraveTheDarkness()); // 不畏苦暗
