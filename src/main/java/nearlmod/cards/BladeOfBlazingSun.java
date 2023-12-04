2ab798a9d68cbbd1463e3394e714305ea5aaa61e
noname512 <zxononame@gmail.com>
2023-12-03 22:39:51 -0500

    部分资源更新

--- src/main/java/nearlmod/cards/BladeOfBlazingSun.java
+++ src/main/java/nearlmod/cards/BladeOfBlazingSun.java
@@ -20,8 +20,7 @@ public class BladeOfBlazingSun extends AbstractNearlCard {
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
-    public static final String[] EXTENDED_DESCRIPTION = cardStrings.EXTENDED_DESCRIPTION;
-    public static final String IMG_PATH = "images/cards/gloriouskazimierz.png";
+    public static final String IMG_PATH = "images/cards/bladeofblazingsun.png";
     private static final int COST = 1;
 
     public BladeOfBlazingSun() {
