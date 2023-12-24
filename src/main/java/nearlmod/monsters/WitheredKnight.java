65afad0fd740abcfd9db81c4e78f4541d5ebbab8
noname512 <zxononame@gmail.com>
2023-12-24 15:18:20 -0500

    “我会成为你的盾”改到第二回合敌人攻击前

--- src/main/java/nearlmod/monsters/WitheredKnight.java
+++ src/main/java/nearlmod/monsters/WitheredKnight.java
@@ -5,12 +5,17 @@ import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.utility.WaitAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.MonsterStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.orbs.AbstractOrb;
 import com.megacrit.cardcrawl.powers.FrailPower;
+import com.megacrit.cardcrawl.vfx.SpeechBubble;
+import nearlmod.orbs.Blemishine;
 import nearlmod.powers.DoubleBossPower;
+import nearlmod.powers.FriendShelterPower;
 
 public class WitheredKnight extends AbstractMonster {
     public static final String ID = "nearlmod:WitheredKnight";
@@ -21,7 +26,8 @@ public class WitheredKnight extends AbstractMonster {
     public static final String IMAGE = "images/monsters/witheredknight.png";
 
     public boolean isCorruptedDead = false;
-    public int SkillTimes = 3;
+    public int skillTimes = 3;
+    public int currentTurn;
 
     public WitheredKnight(float x, float y) {
         super(NAME, ID, 130, 25.0F, 0, 150.0F, 320.0F, IMAGE, x, y);
@@ -31,7 +37,7 @@ public class WitheredKnight extends AbstractMonster {
         if (AbstractDungeon.ascensionLevel >= 18) {
             this.damage.add(new DamageInfo(this, 10));
             this.damage.add(new DamageInfo(this, 16));
-            SkillTimes = 4;
+            skillTimes = 4;
         } else if (AbstractDungeon.ascensionLevel >= 3) {
             this.damage.add(new DamageInfo(this, 9));
             this.damage.add(new DamageInfo(this, 14));
@@ -52,19 +58,30 @@ public class WitheredKnight extends AbstractMonster {
             addToBot(new TalkAction(ms, DIALOG[0], 2.5F, 3.0F));
         addToBot(new TalkAction(this, DIALOG[1], 0.3F, 3.0F));
         addToBot(new ApplyPowerAction(this, this, new DoubleBossPower(this)));
+        currentTurn = 0;
     }
 
     @Override
     public void takeTurn() {
-        addToBot(new ApplyPowerAction(AbstractDungeon.player, this, new FrailPower(AbstractDungeon.player, 1, true)));
+        currentTurn++;
+        AbstractPlayer p = AbstractDungeon.player;
+        if (currentTurn == 2 && !p.hasPower(FriendShelterPower.POWER_ID)) {
+            for (AbstractOrb orb : p.orbs)
+                if (orb instanceof Blemishine) {
+                    AbstractDungeon.effectList.add(new SpeechBubble(orb.hb.cX + 60.0F, orb.hb.cY + 30.0F, 3.0F, DIALOG[3], true));
+                    break;
+                }
+            addToBot(new ApplyPowerAction(p, p, new FriendShelterPower(p)));
+        }
+        addToBot(new ApplyPowerAction(p, this, new FrailPower(p, 1, true)));
         int attTimes = 1;
         if (isCorruptedDead) attTimes++;
         if (this.nextMove == 2) {
             addToBot(new WaitAction(2.0F));
             this.state.setAnimation(0, "Skill", false);
             this.state.addAnimation(0, "Idle", true, 0);
-            for (int i = 0; i < SkillTimes * attTimes; i++) {
-                addToBot(new DamageAction(AbstractDungeon.player, this.damage.get(1)));
+            for (int i = 0; i < skillTimes * attTimes; i++) {
+                addToBot(new DamageAction(p, this.damage.get(1)));
             }
             setMove((byte) 3, Intent.ATTACK_DEBUFF, this.damage.get(0).base, attTimes, (attTimes > 1));
         } else {
@@ -72,12 +89,12 @@ public class WitheredKnight extends AbstractMonster {
             this.state.setAnimation(0, "Attack", false);
             this.state.addAnimation(0, "Idle", true, 0);
             for (int i = 0; i < attTimes; i++) {
-                addToBot(new DamageAction(AbstractDungeon.player, this.damage.get(0)));
+                addToBot(new DamageAction(p, this.damage.get(0)));
             }
             if ((this.nextMove != 7) && (this.nextMove != 1)) {
                 setMove((byte) (this.nextMove + 1), Intent.ATTACK_DEBUFF, this.damage.get(0).base, attTimes, (attTimes > 1));
             } else {
-                setMove(MOVES[0], (byte) 2, Intent.ATTACK_DEBUFF, this.damage.get(1).base, SkillTimes * attTimes, true);
+                setMove(MOVES[0], (byte) 2, Intent.ATTACK_DEBUFF, this.damage.get(1).base, skillTimes * attTimes, true);
             }
         }
     }
@@ -95,7 +112,7 @@ public class WitheredKnight extends AbstractMonster {
         isCorruptedDead = true;
         addToBot(new TalkAction(this, DIALOG[2], 0.3F, 3.0F));
         if (this.nextMove == 2) {
-            setMove(MOVES[0], (byte) 2, Intent.ATTACK_DEBUFF, this.damage.get(1).base, SkillTimes * 2, true);
+            setMove(MOVES[0], (byte) 2, Intent.ATTACK_DEBUFF, this.damage.get(1).base, skillTimes * 2, true);
             this.createIntent();
         }
         else {
