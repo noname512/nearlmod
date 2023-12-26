840622dddf5437f6b37b31178e8b77f18d784713
absi2011 <absi2011@126.com>
2023-12-27 04:13:07 +0800

    鞭刃不触发影bug修复

--- src/main/java/nearlmod/cards/friendcards/WhipSword.java
+++ src/main/java/nearlmod/cards/friendcards/WhipSword.java
@@ -14,7 +14,9 @@ import com.megacrit.cardcrawl.orbs.AbstractOrb;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 import nearlmod.actions.PureDamageAllEnemiesAction;
+import nearlmod.actions.UseShadowAction;
 import nearlmod.orbs.AbstractFriend;
+import nearlmod.orbs.Viviana;
 import nearlmod.patches.AbstractCardEnum;
 
 public class WhipSword extends AbstractFriendCard {
@@ -37,13 +39,26 @@ public class WhipSword extends AbstractFriendCard {
         secondMagicNumber = baseSecondMagicNumber = ATTACK_DMG;
     }
 
+    public int calcShadow() {
+        AbstractPlayer p = AbstractDungeon.player;
+        if (p.hasPower("nearlmod:Shadow")) {
+            return p.getPower("nearlmod:Shadow").amount;
+        }
+        return 0;
+    }
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         addToBot(new DamageAllEnemiesAction(p, secondMagicNumber, DamageInfo.DamageType.NORMAL, AbstractGameAction.AttackEffect.NONE));
 
         for (AbstractOrb orb : p.orbs)
             if (orb instanceof AbstractFriend) {
-                addToBot(new PureDamageAllEnemiesAction(p, secondMagicNumber + ((AbstractFriend)orb).getTrustAmount(), orb.ID + damageSuffix));
+                int friendDamage = secondMagicNumber + ((AbstractFriend) orb).getTrustAmount();
+                if (orb instanceof Viviana) {
+                    friendDamage += calcShadow();
+                    addToBot(new UseShadowAction(p));
+                }
+                addToBot(new PureDamageAllEnemiesAction(p, friendDamage, orb.ID + damageSuffix));
+
             }
 
         if (upgraded) {
@@ -67,6 +82,9 @@ public class WhipSword extends AbstractFriendCard {
         for (AbstractOrb orb : p.orbs)
             if (orb instanceof AbstractFriend) {
                 damage += secondMagicNumber + ((AbstractFriend)orb).getTrustAmount();
+                if (orb instanceof Viviana) {
+                    damage += calcShadow();
+                }
             }
         float tmp = secondMagicNumber;
         for (AbstractRelic relic : p.relics)
@@ -97,7 +115,11 @@ public class WhipSword extends AbstractFriendCard {
         AbstractPlayer p = AbstractDungeon.player;
         for (AbstractOrb orb : p.orbs)
             if (orb instanceof AbstractFriend) {
-                damage += staticCalcDmg(mo, secondMagicNumber + ((AbstractFriend)orb).getTrustAmount(), damageTypeForTurn, true);
+                int friendDamage = secondMagicNumber + ((AbstractFriend)orb).getTrustAmount();
+                if (orb instanceof Viviana) {
+                    friendDamage += calcShadow();
+                }
+                damage += staticCalcDmg(mo, friendDamage, damageTypeForTurn, true);
             }
         DamageInfo t = new DamageInfo(p, secondMagicNumber);
         t.applyPowers(p, mo);
