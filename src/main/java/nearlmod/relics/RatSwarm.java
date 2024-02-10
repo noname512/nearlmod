1f76df01dd7a3fc0816e18da6f13a5bddab2a1fc
absi2011 <absi2011@126.com>
2024-02-10 16:25:16 +0800

    鼠群(除图)，以及bug修复(盾与避难所削弱后仍然被认为是获得光的牌)

---
+++ src/main/java/nearlmod/relics/RatSwarm.java
@@ -0,0 +1,36 @@
+package nearlmod.relics;
+
+import basemod.abstracts.CustomRelic;
+import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.RelicStrings;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+
+public class Lighthouse extends CustomRelic {
+
+    public static final String ID = "nearlmod:Lighthouse";
+    public static final RelicStrings relicStrings = CardCrawlGame.languagePack.getRelicStrings(ID);
+    public static final String NAME = relicStrings.NAME;
+    public static final String[] DESCRIPTIONS = relicStrings.DESCRIPTIONS;
+    public static final Texture IMG = new Texture("images/relics/lighthouse.png");
+    public static final Texture IMG_OUTLINE = new Texture("images/relics/lighthouse_p.png");
+    public static boolean isFirstTime;
+    public Lighthouse() {
+        super(ID, IMG, IMG_OUTLINE, RelicTier.BOSS, LandingSound.SOLID);
+    }
+
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0];
+    }
+
+    @Override
+    public void atTurnStart() {
+        isFirstTime = true;
+    }
+
+    @Override
+    public AbstractRelic makeCopy() {
+        return new Lighthouse();
+    }
+}
