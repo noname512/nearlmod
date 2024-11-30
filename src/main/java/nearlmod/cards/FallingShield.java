0c67e1ae1773ff6f1d68c28a44bcf82c2adbea84
absi2011 <absi2011@126.com>
2024-12-01 03:47:48 +0800

    坠落的盾 bug修复(?) & 感觉需要重做下

--- src/main/java/nearlmod/cards/FallingShield.java
+++ src/main/java/nearlmod/cards/FallingShield.java
@@ -4,15 +4,20 @@ import com.evacipated.cardcrawl.mod.stslib.patches.core.AbstractCreature.TempHPF
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.relics.ChemicalX;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import nearlmod.NLMOD;
 import nearlmod.actions.FallingShieldAction;
 import nearlmod.characters.Nearl;
 import nearlmod.orbs.Horn;
 import nearlmod.patches.AbstractCardEnum;
 import nearlmod.patches.NearlTags;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 public class FallingShield extends AbstractNearlCard {
     public static final String ID = "nearlmod:FallingShield";
@@ -25,6 +30,7 @@ public class FallingShield extends AbstractNearlCard {
     private static final int ATTACK_TIMES = 2;
     private static final int EXTRA_TIMES = 2;
     private static final int UPGRADE_PLUS_TIME = 1;
+    private static final Logger logger = LogManager.getLogger(NLMOD.class.getName());
 
     public FallingShield() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
@@ -60,11 +66,17 @@ public class FallingShield extends AbstractNearlCard {
         } else {
             summon = true;
         }
-        if (freeToPlay() || isInAutoplay) {
+        if (isInAutoplay) {
             tempHp = 0;
         }
+        if (AbstractDungeon.player != null && AbstractDungeon.currMapNode != null && AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT && AbstractDungeon.player.hasPower("FreeAttackPower") && this.type == AbstractCard.CardType.ATTACK) {
+            tempHp = 0;
+        }
+        // TODO: 目前无法相应死灵书等，考虑下要不要干脆改描述算了（例如消耗你所有的临时生命，造成等量伤害2次，如果号角在场，再造成2次，否则召唤，类似于这种写法）
+        // TODO: 感觉这个bug是修不完的。直接破坏原有逻辑了（或者大力patch，还是算了吧）
         addToBot(new FallingShieldAction(tempHp, damage, times, summon, m));
-        costForTurn = energyOnUse = 0;
+        costForTurn = 0;
+        energyOnUse = 0;
     }
 
     @Override
