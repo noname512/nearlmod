f248b5f1972f224172576822275433ca6f8a33f6
noname512 <zxononame@gmail.com>
2025-01-01 07:44:46 -0500

    荆棘环身触发时机调整

--- src/main/java/nearlmod/powers/WreathedInThornsPower.java
+++ src/main/java/nearlmod/powers/WreathedInThornsPower.java
@@ -3,10 +3,12 @@ package nearlmod.powers;
 import basemod.interfaces.CloneablePowerInterface;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.evacipated.cardcrawl.mod.stslib.patches.core.AbstractCreature.TempHPField;
-import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.OnLoseTempHpPower;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -17,7 +19,7 @@ import com.megacrit.cardcrawl.powers.AbstractPower;
 import nearlmod.cards.friendcards.AbstractFriendCard;
 import nearlmod.orbs.Penance;
 
-public class WreathedInThornsPower extends AbstractPower implements CloneablePowerInterface, OnLoseTempHpPower {
+public class WreathedInThornsPower extends AbstractPower implements CloneablePowerInterface {
     public static final String POWER_ID = "nearlmod:WreathedInThornsPower";
     public static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
@@ -39,23 +41,26 @@ public class WreathedInThornsPower extends AbstractPower implements CloneablePow
         description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
     }
 
-    @Override
-    public int onLoseTempHp(DamageInfo info, int damage) {
-        if (info.type == DamageInfo.DamageType.NORMAL && info.owner != null && info.owner != owner && TempHPField.tempHp.get(owner) > 0) {
-            int trust = -1;
-            for (AbstractOrb o : AbstractDungeon.player.orbs)
-                if (o instanceof Penance) {
-                    trust = ((Penance) o).getTrustAmount();
-                    break;
+    @SpirePatch(clz = AbstractPlayer.class, method = "damage")
+    public static class OnDamagedPatch {
+        @SpirePrefixPatch
+        public static void Prefix(AbstractPlayer _inst, DamageInfo info) {
+            if (_inst.hasPower(WreathedInThornsPower.POWER_ID) && TempHPField.tempHp.get(_inst) > 0 &&
+                    info.type == DamageInfo.DamageType.NORMAL && info.owner != null && info.owner != _inst) {
+                int trust = -1;
+                for (AbstractOrb o : AbstractDungeon.player.orbs)
+                    if (o instanceof Penance) {
+                        trust = ((Penance) o).getTrustAmount();
+                        break;
+                    }
+                if (trust != -1) {
+                    _inst.getPower(WreathedInThornsPower.POWER_ID).flash();
+                    DamageInfo newInfo = new DamageInfo(null, _inst.getPower(WreathedInThornsPower.POWER_ID).amount + trust, DamageInfo.DamageType.THORNS);
+                    newInfo.name = Penance.ORB_ID + AbstractFriendCard.damageSuffix;
+                    AbstractDungeon.actionManager.addToTop(new DamageAction(info.owner, newInfo, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
                 }
-            if (trust != -1) {
-                flash();
-                DamageInfo newInfo = new DamageInfo(null, amount + trust, DamageInfo.DamageType.THORNS);
-                newInfo.name = Penance.ORB_ID + AbstractFriendCard.damageSuffix;
-                addToTop(new DamageAction(info.owner, newInfo, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
             }
         }
-        return damage;
     }
 
     @Override
